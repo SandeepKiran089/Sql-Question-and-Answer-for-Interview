17195432eb93d6ffb8ec88fc3ac33a9b2b55437a
SandeepKiran089 <55267760+SandeepKiran089@users.noreply.github.com>
2021-08-03 15:02:30 +0530

    Update Interview_Pre.sql

--- Interview_Pre.sql
+++ Interview_Pre.sql
@@ -1,11 +1,23 @@
+# Instructions to setup mysql on your local computer if you have not setup yet.
+1) Follow step in this video to install mysql on your local computer https://www.youtube.com/watch?v=WuBcTJnIuzo
+
+2) SQL database which need to be dump into your local system. Use hr_dbms.sql file above. Download hr_dbms.sql file to your local computer and import it as per 
+instructions given in the tutorial video
+
+To use database use below commands
+
 use hr_dbms;
 SHOW databases;
 show table status;
 show tables;
 
+Show all employee records
+
 SELECT * FROM hr_dbms.employees;
 
+
 # Write a query to extraxct the domain name from the email column
+
 SELECT instr(email,'@') from hr_dbms.employees;
 select substr(email,instr(email,'@')+1) from hr_dbms.employees;
 
@@ -29,6 +41,7 @@ where dt.department_name='IT' and cr.country_name='United States of America';
 
 
 # Write a query to fetch the region_name and country_name
+
 select region_name,country_name 
 from (
 select region_name, country_name from hr_dbms.regions re
@@ -44,6 +57,7 @@ and country_name='India') as rgn;
 # FROM, SELECT, LIMIT, WHERE
 
 use classicmodels;
+
 select * from classicmodels.customers where country='USA';
 select max(creditLimit) from classicmodels.customers;
 select * from classicmodels.payments;
@@ -58,8 +72,11 @@ SELECT c.customerName, p.checkNumber,p.amount ,sum(c.creditLimit)
  HAVING p.amount<max(c.creditLimit)
  ORDER BY c.customerName
  LIMIT 5;
+ 
 use hr_dbms;
+
 # Fetch data of all employee who is from same department
+
 select * from hr_dbms.employees;
 select * from hr_dbms.departments;
 
@@ -70,18 +87,22 @@ and e1.employee_id != e.employee_id
 limit 5;
 
 # Fetch the emaployee details from employee table who joined the organization in year 2021
+
 select * from hr_dbms.employees;
 select employee_id,first_name,last_name, hire_date from hr_dbms.employees
 where hire_date between '2000-01-01' and '2000-12-31';
 
 select * from hr_dbms.employees;
+
 # Write an SQL query to fetch duplicate records from EmployeeDetails (without considering the primary key – EmpId).
+
 select first_name,last_name,email,phone_number,hire_date,salary,count(*)
 from hr_dbms.employees
 group by first_name,last_name,email,phone_number,hire_date,salary
 having count(*)>1;
 
 # Write an SQL query to fetch only odd rows from the table.
+
 /*  MySQL ROW_NUMBER() Function
 	The ROW_NUMBER() function in MySQL is used to returns the sequential number for each row 
     within its partition. It is a kind of window function. The row number starts from 1 to the 
